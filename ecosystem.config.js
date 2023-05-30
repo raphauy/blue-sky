b9bab433cc9cc540211b3b4621106ef9b0503d59
raphauy <rapha.uy@rapha.uy>
2023-05-30 17:14:42 -0300

    First commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-    apps: [
-      {
-        name: "blue-sky",
-        script: "pnpm",
-        args: "run start -- -p 8053",
-      },
-    ],
-  };
- 
+  apps: [
+    {
+      name: "blue-sky",
+      script: "pnpm",
+      args: "run start -- -p 8053",
+      interpreter: "none",
+      cwd: "/var/www/rapha/blue-sky", // Ruta de trabajo actual de tu aplicación
+    },
+  ],
+};
